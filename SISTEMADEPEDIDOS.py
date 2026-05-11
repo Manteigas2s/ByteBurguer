3c9a811107507db82f7751949be02d627495efb0
Manteigas2s <ph5154671@gmail.com>
2026-05-11 20:07:29 -0300

    Update SISTEMADEPEDIDOS.py

--- SISTEMADEPEDIDOS.py
+++ SISTEMADEPEDIDOS.py
@@ -8,18 +8,25 @@ pedidos_itens = []
 pedidos_precos = []
 
 while True:
-    print("===== CARDÁPIO =====")
+    print()
+    print("===== CARDÁPIO =====\n")
     for i, item in enumerate(itens):
         print(f"{i+1}. {item} ........ R$ {precos[i]:.2f}")
         
-    nome_cliente = input("Digite seu nome: ")
-    clientes.append(nome_cliente)
+    nome_cliente = input("\nDigite seu nome: ")
+    
     if nome_cliente == "fim":
         break
     else:
         print("Obrigado!")
+    
+    clientes.append(nome_cliente)
+    pedidos_itens = []
+    pedidos_precos = []
+
 
     while True:
+        
         pedido_cliente = int(input("Digite o número do item desejado: "))
         if pedido_cliente == 0:
             break
@@ -32,5 +39,21 @@ while True:
 
     print(f"TOTAL: R$ {soma:.2f}")
     faturamento += soma
+
+    remover = input("Deseja remover algum item? (Sim/Não)")
+    if remover == "sim":
+        num = print(input("Digite o número do item: "))
+        num - 1
+
+print(f"""
+------ RELATÓRIO FINAL ------
+      
+    Total de clientes: {len(clientes)}
+    Nome de todos os clientes: {clientes}
+    Item mais caro: {max(pedidos_precos):.2f}
+    Item mais barato: {min(pedidos_precos):.2f}
+    Faturamento total: {faturamento:.2f}
+""")
+
 
 
